8645a77d2d458a1217cc00c80e00603453bb6af7
AlexanderDev <brioneselias2019@gmail.com>
2024-11-04 21:12:52 -0300

    Agregando los atributos con ForeignKey para area y cargo. Ademas de dividir el nombre del contacto

--- WebApp/models.py
+++ WebApp/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# Modelo para áreas/departamentos
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    numero = models.PositiveIntegerField()  # Número del departamento, puede ser usado como un identificador único o número de piso
+
+    def __str__(self):
+        return f"{self.nombre} (Nº {self.numero})"
+
+# Modelo de Área y referencia al departamento
+class Area(models.Model):
+    nombre = models.CharField(max_length=50)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, related_name="areas")
+
+    def __str__(self):
+        return f"{self.nombre} - {self.departamento.nombre}"
+
+# Modelo de Cargo, ahora depende de Área
+class Cargo(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, related_name="cargos")  # Relación con Área
+
+    def __str__(self):
+        return self.nombre
+
 # Modelo personalizado de usuario que hereda de AbstractUser
 class Trabajador(AbstractUser):
     RUT = models.CharField(max_length=12, unique=True)
@@ -9,8 +33,9 @@ class Trabajador(AbstractUser):
     email = models.EmailField(max_length=254, unique=True, blank=True) 
     sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Femenino')])
     fecha_ingreso = models.DateField()
-    cargo = models.CharField(max_length=50)
-    area = models.CharField(max_length=50)
+    cargo = models.ForeignKey(Cargo, on_delete=models.SET_NULL, null=True, related_name="trabajadores")
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True, related_name="trabajadores")
+    departamento = models.ForeignKey(Departamento, on_delete=models.SET_NULL, null=True, related_name="trabajadores")
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name} ({self.RUT})"
@@ -19,6 +44,7 @@ class Trabajador(AbstractUser):
 class ContactoEmergencia(models.Model):
     trabajador = models.ForeignKey(Trabajador, on_delete=models.CASCADE, related_name="contactos_emergencia")
     nombre_contacto = models.CharField(max_length=100)
+    apellido_contacto = models.CharField(max_length=100)
     relacion = models.CharField(max_length=50)
     telefono_contacto = models.CharField(max_length=15)
 
@@ -35,10 +61,3 @@ class CargaFamiliar(models.Model):
 
     def __str__(self):
         return f"{self.nombre_carga} - {self.parentesco}"
-
-# Modelo para áreas/departamentos
-class Departamento(models.Model):
-    nombre = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.nombre
