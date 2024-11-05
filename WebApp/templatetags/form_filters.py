f4e06456df324dd38eccd5d67a4067dee154a461
AlexanderDev <brioneselias2019@gmail.com>
2024-11-04 21:52:20 -0300

    Arreglando un fallo de la logica

--- WebApp/templatetags/form_filters.py
+++ WebApp/templatetags/form_filters.py
@@ -2,6 +2,11 @@ from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='add_class')
 def add_class(field, css_class):
-    return field.as_widget(attrs={"class": css_class})
+    # Verificar si el campo tiene el método as_widget, que indica que es un campo de formulario
+    if hasattr(field, 'as_widget'):
+        return field.as_widget(attrs={'class': css_class})
+    # Si no es un campo de formulario, simplemente devuelve el valor original
+    return field
+
